5e3147887b08eb8a5dc1488ffefe8901b843a3f8
Martin Marinov <martinmarinov@Martins-MacBook-Air.local>
2023-12-27 18:12:12 +0200

    Resolve conflict

--- src/main.py
+++ src/main.py
@@ -1,32 +1,11 @@
-import os
-import json
-import asyncio
-
-from dotenv import load_dotenv
-
-from libs.movie_api import MovieAPI
-from libs.movie_api import TimeWindow
-
+# main.py
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-async def get_trending_movies():
-    load_dotenv()
-    movie_api = MovieAPI(os.environ["MOVIE_API_KEY"])
-    media_data = await movie_api.get_trending_movies(TimeWindow.DAY)
-    media_data.sort(key=lambda x: x.rating, reverse=True)
-    return json.dumps([json.loads(media.serialize()) for media in media_data])
-
-
-@app.route("/")
-def trending_movies():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    movies = loop.run_until_complete(get_trending_movies())
-    return movies
-
-
-if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
